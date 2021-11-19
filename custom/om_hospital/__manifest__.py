620b0453590a2337728d6a7b6aaba92e4009fc79
Md. Shihab Uddin <piash222@gmail.com>
2021-11-20 00:57:12 +0600

    1. create custom module in odoo

---
+++ custom/om_hospital/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': 'Hospital Management',
+    'version': '13.0.1.0.0',
+    'summary': 'Module for managing the Hospital',
+    'description': 'Description',
+    'sequence': '10',
+    'category': 'Extra Tools',
+    'author': 'Md. Shihab Uddin',
+    'website': 'Website',
+    'license': 'LGPL-3',
+    'depends': [
+
+    ],
+    'data': [
+
+    ],
+    'demo': [
+
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False
+}
