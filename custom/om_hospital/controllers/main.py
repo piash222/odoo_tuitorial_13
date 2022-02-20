272ed7ef3360b43e8e9b042dbf89e58a1c5a96b0
Md. Shihab Uddin <piash222@gmail.com>
2022-02-20 08:15:15 +0600

    77. Odoo Web Controllers Auth Public and Auth User Difference

--- custom/om_hospital/controllers/main.py
+++ custom/om_hospital/controllers/main.py
@@ -5,6 +5,8 @@ from odoo.http import request
 class Hospital(http.Controller):
 
     # sample controller created
+    # auth='user' >> webpage visible for authenticated logged in user
+    # auth='public' >> webpage visible for all including not logged in
     @http.route('/hospital/patient/', website=True, auth='public')
     def hospital_patient(self):
         patients = request.env['hospital.patient'].sudo().search([])
