aea19c304ddc454a88794c81c2324236935417b2
Md. Shihab Uddin <piash222@gmail.com>
2022-03-13 23:38:17 +0600

    80. Odoo Mobile App Development - Authentication, Fetching Data & Creating Records Using Controller

--- custom/om_hospital/controllers/main.py
+++ custom/om_hospital/controllers/main.py
@@ -13,3 +13,30 @@ class Hospital(http.Controller):
         print(patients)
         # return "Hello World"
         return request.render('om_hospital.patient_page', {'patients': patients})
+
+    @http.route('/get_patients', type='json', auth='user')
+    def get_patients(self):
+        patients_rec = request.env['hospital.patient'].search([])
+        patients = []
+        for rec in patients_rec:
+            vals = {
+                'id': rec.id,
+                'name': rec.patient_name
+            }
+            patients.append(vals)
+        data = {
+            'status': 200, 'response': patients, 'message': 'success'
+        }
+        return data
+
+    @http.route("/create_patients",type='json', auth='user')
+    def create_patients(self, **rec):
+        if request.jsonrequest:
+            print("rec", rec)
+            if rec["name"]:
+                vals = {
+                    'patient_name': rec['name']
+                }
+                new_patient = request.env['hospital.patient'].sudo().create(vals)
+                args = {"success": True, "ID": new_patient.id}
+            return args
